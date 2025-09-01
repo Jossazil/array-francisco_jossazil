4e3c912c2d6b504757a347d26e4168fe3fba66e3
Francisco Jossazil <fjossazil@gmail.com>
2025-09-01 19:54:41 -0300

    Update array.py

--- array.py
+++ array.py
@@ -1,1 +1,11 @@
+alunos = ['Marcos', 'Fernando', 'Jose', 'Fabio', 'Tulio']
+nome = 'Carla'
 
+print(alunos[0])
+print(alunos[2])
+print(alunos[4])
+
+if nome in alunos:
+    print("Carla está na lista!")
+else:
+    print("Carla não foi encontrada na lista") 
